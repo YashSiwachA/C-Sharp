edaff09f5a0f449c371c74bf7285036fb648c291
Yash Siwach <yashsiwach07@gmail.com>
2024-05-08 20:47:39 +0530

    M

--- Practice/Hello.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace Hello{
-    class HelloW{
-        public static void Main(string[] args){
-            Console.WriteLine("HelloWorld!!!!.....");
-            Console.ReadKey();
-        }
-    }
-}
