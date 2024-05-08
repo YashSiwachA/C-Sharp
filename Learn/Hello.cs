79a41fb61344c4d319dc940a4d2e56a7d9c5944e
Yash Siwach <yashsiwach07@gmail.com>
2024-05-08 20:46:34 +0530

    Learn is Added and rest are deleted;

---
+++ Learn/Hello.cs
@@ -0,0 +1,11 @@
+using System;
+namespace HelloWorld{
+    class HelloWorld{
+        public static void Main(string[] args){
+            string a = "Hello To This World!!!!.....";
+            Console.WriteLine(a);
+            Console.ReadKey();
+            
+        }
+    }
+}
